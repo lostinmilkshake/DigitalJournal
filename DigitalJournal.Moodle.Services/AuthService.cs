2751fd9751a6a3cfcfe9e31b8b6ef2c0875b11d5
Kirill <kirillitvinov@gmail.com>
2021-04-26 21:53:31 +0300

    Added Auth Service

--- DigitalJournal.Moodle.Services/AuthService.cs
+++ DigitalJournal.Moodle.Services/AuthService.cs
@@ -1,10 +1,21 @@
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using DigitalJournal.Moodle.Domain;
+using DigitalJournal.Moodle.Services.Interfaces;
+
 namespace DigitalJournal.Moodle.Services
 {
-    public class AuthService
+    public class AuthService : IAuthService
     {
-        public void Login(string username, string password)
+        public async Task<AuthResponse> Login(string username, string password)
         {
-            
+            var query =
+                $"http://localhost:80/login/token.php?username={username}&password={password}&service=moodle_mobile_app";
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetAsync(query);
+            var result = await response.Content.ReadAsStringAsync();
+            return JsonSerializer.Deserialize<AuthResponse>(result);
         }
     }
 }
---
+++ DigitalJournal.Moodle.Services/Interfaces/IAuthService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using DigitalJournal.Moodle.Domain;
+
+namespace DigitalJournal.Moodle.Services.Interfaces
+{
+    public interface IAuthService
+    {
+        public Task<AuthResponse> Login(string username, string password);
+    }
+}
