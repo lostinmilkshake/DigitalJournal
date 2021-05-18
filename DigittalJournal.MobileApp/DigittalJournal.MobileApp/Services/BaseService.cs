1183f6958f88179d89edc731e615f6ab8af396b7
Dina The Dinosaur <kirillitvinov@gmail.com>
2021-05-18 20:49:55 +0300

    Added VKR

--- DigittalJournal.MobileApp/DigittalJournal.MobileApp/Services/BaseService.cs
+++ DigittalJournal.MobileApp/DigittalJournal.MobileApp/Services/BaseService.cs
@@ -9,7 +9,7 @@ namespace DigittalJournal.MobileApp.Services
     public class BaseService
     {
         // TODO: Change BaseUrl
-        private string BaseUrl = "http://e92f2bff8f8f.ngrok.io/";
+        private string BaseUrl = "http://a5fb0825b940.ngrok.io/";
 
         protected readonly HttpClient _httpClient;
         protected readonly JsonSerializerOptions _jsonSerializerOptions;
